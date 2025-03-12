a47d185d03b44ffea0f2628e01f9e5c642725e55
Komodo96 <gct005@student.usc.edu.au>
2025-03-12 14:44:19 +1000

    Commit #1
    Made changes to the assets in Unity Editor such as replacing the sphere to a cube as well as adding collision to the walls and fixing the pickups. Also began fixing the code in Visual Studio

---
+++ SGD213 1A/Assets/RollABall/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // Create private variables for player speed, and for the Text UI game objects
+    [SerializeField]
+    private float playerSpeed;
+
+    public Text scoreText;
+    public Text winText;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+	private int score;
+
+	// At the start of the game..
+	void Start ()	
+    {
+		rb = GetComponent<Rigidbody>();
+		score = 0;
+		SetScoreText();
+		winText.text = "";
+    }
+
+	void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * playerSpeed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+{
+if (other.gameObject.CompareTag ("Pick Up"))
+{						    other.gameObject.SetActive (false);
+							score = score + 1;
+							SetScoreText ();
+    }}
+
+    void SetScoreText()
+    {
+		if (score == 0) return;  
+		scoreText.text = "Score: " + score.ToString ();
+		if (score >= 12) 
+        {
+			// Set the text value of our 'winText'
+			winText.text = "You Win!";
+        }
+    }
+}
