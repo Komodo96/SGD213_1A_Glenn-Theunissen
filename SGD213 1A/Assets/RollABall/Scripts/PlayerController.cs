008925924b7174ab2d3acfd318af323d9cc004b5
Komodo96 <gct005@student.usc.edu.au>
2025-03-13 13:48:36 +1000

    Commit #2
    Fixed up more of the formatting and layout of the code in Visual Studio. Added more comments to make the code more easy to read and started working on making the game over screen more interesting by adding a background. Button to restart the game to be added.

--- SGD213 1A/Assets/RollABall/Scripts/PlayerController.cs
+++ SGD213 1A/Assets/RollABall/Scripts/PlayerController.cs
@@ -7,52 +7,96 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    // Create private variables for player speed, and for the Text UI game objects
     [SerializeField]
+//Variable for how fast the player moves
     private float playerSpeed;
 
+//Variable for the UI text that will display the score on screen
     public Text scoreText;
+	
+//Variable for the UI text that will display the game winning screen
     public Text winText;
 
-	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+// Rigidbody component for the player
 	private Rigidbody rb;
+
+//Variable to keep track of how many objects the player has picked up
 	private int score;
 
-	// At the start of the game..
+//Variable to show Game Over UI screen 
+    public GameObject gameOverUI;
+
+// At the start of the game, before the first frame update 
 	void Start ()	
     {
-		rb = GetComponent<Rigidbody>();
+//call the rigidbody component which is attatched to the player
+        rb = GetComponent<Rigidbody>();
+
+//Set the default starting score to 0
 		score = 0;
+
+//Update the score displayed by the UI
 		SetScoreText();
+
+//Set the win text to inactive by default
 		winText.text = "";
     }
 
+//Called once per frame update
 	void FixedUpdate ()
 	{
-		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+// Set local float variables equal to the value of our Horizontal and Vertical Inputs
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+
+// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+//Apply force the the rigidbody component of the player
 		rb.AddForce (movement * playerSpeed);
 	}
 
-	void OnTriggerEnter(Collider other) 
-{
-if (other.gameObject.CompareTag ("Pick Up"))
-{						    other.gameObject.SetActive (false);
-							score = score + 1;
-							SetScoreText ();
-    }}
+//Called when the player collides with another object with collision active
+    void OnTriggerEnter(Collider other) 
+    {
+//If the player collides with an object designated as a pick up..  
+        if (other.gameObject.CompareTag ("Pick Up"))
 
+//Remove the pickup from the scene..
+        {	other.gameObject.SetActive (false);
+
+//Increment the score by 1.
+            score++;
+
+//Then call the SetScoreText function
+            SetScoreText();
+        }
+	}
+
+//Function used to set the game over UI screen to Active
+    void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+//Function Used to upadate and display the score on the players screen and check if the player has collected all pickups. If so then display the Win Text on screen
     void SetScoreText()
     {
-		if (score == 0) return;  
-		scoreText.text = "Score: " + score.ToString ();
-		if (score >= 12) 
+//Check if the score is currently 0 and if so then return from this function 
+		if (score == 0) return;
+
+//Get the current value of the score variable and display it on the player's screen
+        scoreText.text = "Score: " + score.ToString ();
+
+//If the score reaches 12 then... 
+        if (score >= 12) 
         {
-			// Set the text value of our 'winText'
-			winText.text = "You Win!";
+
+//Call the Game Over function to activate the UI on the player's screen 
+            GameOver();
+
+//set the text value of our 'winText' to active
+            winText.text = "You Win!";
         }
     }
 }
